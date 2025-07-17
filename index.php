32a8fe6ec0e6590507ac153bb9d0b1543a82e71c
bernurgiraz <bernur.giraz@tedu.edu.tr>
2025-07-17 15:43:00 +0300

    düzeltme

--- index.php
+++ index.php
@@ -2,30 +2,9 @@
 include_once("lib/fonksiyon.php");
 
 $sinif=new kurumsal;
-<!-- : -->
 
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
