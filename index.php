80757a1c2654a9dc1c4f2ec2c98b96f12616510d
bernurgiraz <bernur.giraz@tedu.edu.tr>
2025-07-17 15:11:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,8 @@ $sinif=new kurumsal;
     <i class="fa fa-envelope-o"></i><a href="mailto:<?php echo $sinif->mailadres; ?>"><?php echo $sinif->mailadres; ?></a>
     <i class="fa fa-phone"></i><?php echo $sinif->telno; ?>    
     
-    </div>    
+    </div>      
+    <div class="social-links float-left">    
     <a href="<?php echo $sinif->tvit; ?>" class="twitter"><i class="fa fa-twitter"></i></a>
      <a href="<?php echo $sinif->face; ?>" class="facebook"><i class="fa fa-facebook"></i></a>
       <a href="<?php echo $sinif->ints; ?>" class="instagram"><i class="fa fa-instagram"></i></a> 
@@ -73,6 +74,7 @@ $sinif=new kurumsal;
 </section> 
 
 <!--yenilik-->
+
 
 <!-- header -->
 
